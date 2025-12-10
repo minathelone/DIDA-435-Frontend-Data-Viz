6645f03f8b8de1550ee8e08ec7cf0f1cec661320
minathelone <minaparklone@gmail.com>
2025-12-10 10:42:18 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { createWaterfallPlot } from './Waterfall/index.js';
 export { createButterflyChart } from './Butterfly/index.js';
 export { createRadicalHistogram } from './Radial Histogram/index.js';
+export { createPictorialGraph } from './Pictorial Graphs/index.js';
